2c56bfeed05ec447938983638186d25a1818740b
bivector <zvonkov.dalv@gmail.com>
2026-02-11 23:06:58 +0800

    restore to C++ WinAPI

---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <windows.h>
+const int scrWidth = GetSystemMetrics(SM_CXSCREEN);
+const int scrHeight = GetSystemMetrics(SM_CYSCREEN);
+
+const int wWidth = 800;
+const int wHeight = 600;
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+{
+
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"Breakout game";
+
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Breakout game",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        (scrWidth - wWidth) / 2, (scrHeight - wHeight) / 2, wWidth, wHeight,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        RECT textArea;
+        LPCTSTR text = L"Hello, Breakout";
+        const int len = lstrlen(text);
+        GetClientRect(hwnd, &textArea);
+        DrawText(hdc, text, len, &textArea, DT_CENTER | DT_VCENTER);
+
+        EndPaint(hwnd, &ps);
+    }
+    return 0;
+    }
+
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
