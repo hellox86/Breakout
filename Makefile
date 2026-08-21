dbb7fa4de493f6685415f8fce768e6f15e2fa786
bivector <zvonkov.dalv@gmail.com>
2026-08-21 17:25:07 +0800

    ball update

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
 all:
 	nasm -f bin breakout.asm -o breakout.bin
-	qemu-system-i386 breakout.bin
+	qemu-system-i386 -audiodev dsound,id=out -monitor stdio breakout.bin
--- breakout.asm
+++ breakout.asm
@@ -4,9 +4,10 @@ bits 16
 OFFSET EQU 0xA000
 WIDTH EQU 320
 HEIGHT EQU 200
-PADDLE_WIDTH EQU 20
+PADDLE_WIDTH EQU 16
 PADDLE_HEIGHT EQU 7
-
+BALL_RADIUS EQU 4
+	
 start: 
 	mov ax, 0x13
 	int 0x10	
@@ -17,11 +18,13 @@ start:
 	mov word [padx], 160
 	mov word [pady], 180
 	mov word [ballx], 160
-	mov word [bally], 170
-
+	mov word [bally], 174
+	mov word [bdx], 4
+	mov word [bdy], 4
 	mov dword [0x0070], draw
 	
 .loop:
+    hlt
     mov ah, 0x01
     int 0x16
 
@@ -48,11 +51,11 @@ start:
 
 
 .left:
-	mov word [pdx], -10
+	mov word [pdx], -5
 	jmp .no_key
 	
 .right:
-	mov word [pdx], 10	
+	mov word [pdx], 5	
 	jmp .no_key
 
 .no_key:
@@ -63,8 +66,18 @@ draw_paddle:
 	mov bx, [padx]
 	sub bx, PADDLE_WIDTH>>1
 	mov cx, PADDLE_HEIGHT
+	mov dh, PADDLE_WIDTH
 	call rowloop
 	ret
+draw_ball:
+	mov ax, [bally]
+	mov bx, [ballx]
+	sub bx, BALL_RADIUS>>1
+	mov cx, BALL_RADIUS
+	mov dh, BALL_RADIUS
+	call rowloop
+	ret
+
 clear_scr:
 	pusha
 	mov ax, OFFSET
@@ -87,16 +100,18 @@ rowx:
 	
 	add ax, bx
 	mov di, ax
-	
+
 	mov al, dl
-	mov cx, PADDLE_WIDTH
-	rep stosb
 	pop cx
+	mov cl, dh
+	xor ch, ch
+	
+	rep stosb
 	pop di
 	pop bx
 	pop ax
 	ret
-	
+
 rowloop:
 	push cx
 	call rowx
@@ -104,32 +119,111 @@ rowloop:
 	inc ax
 	loop rowloop
 	ret
-	
+
+;; old hack
+bounce:
+	mov ax, [ballx]
+	sub ax, PADDLE_WIDTH>>1
+	cmp ax, 0
+	jge .div
+	add ax, 3
+.div:
+	sar ax, 2
+	add ax, [bdx]
+	neg word [bdy]
+	ret
+
 
 update:
-	cmp word [padx], PADDLE_WIDTH>>1
+	cmp word [padx], PADDLE_WIDTH
 	jle .neg_pad
-	cmp word [padx], WIDTH - (PADDLE_WIDTH>>1)
+	cmp word [padx], WIDTH - PADDLE_WIDTH
 	jge .neg_pad
+
+	cmp word [ballx], 0
+	jle .neg_ballx
+	cmp word [ballx], WIDTH - (BALL_RADIUS)
+	jge .neg_ballx
+
+	cmp word [bally], 0
+	jle .neg_bally
+	cmp word [bally], HEIGHT - (BALL_RADIUS)
+	jge .neg_bally
+	call col
+	
+.uball:
+	mov ax, [ballx]
+	add ax, [bdx]
+	mov [ballx], ax
+	xor ax, ax
+	
+	mov ax, [bally]
+	add ax, [bdy]
+	mov [bally], ax
 
 .upad:
 	mov ax, [padx]
 	add ax, [pdx]
 	mov [padx], ax
-	ret
-	
+	ret	
 .neg_pad:
 	neg word [pdx]
 	jmp .upad 
+.neg_ballx:
+	neg word [bdx]
+	jmp .uball
+.neg_bally:
+	neg word [bdy]
+	jmp .uball
 
+col:
+	;; optimized collision 
+	mov ax, [ballx]
+	sub ax, [padx]
+	cmp ax, PADDLE_WIDTH + BALL_RADIUS
+	jg .end
+	add ax, BALL_RADIUS
+	jle .end
+
+	mov ax, [bally]
+	sub ax, [pady]
+	cmp ax, PADDLE_HEIGHT + BALL_RADIUS
+	jg .end
+	add ax, BALL_RADIUS
+	jle .end
+	jmp .hand
+	
+.hand:
+	call bounce
+	call sound
+.end:
+	ret
+sound:
+	pusha
+	mov dx, 0x61
+	mov al, 0x02
+	out dx, al
+
+	mov cx, 1000
+	.delay:
+		dec cx
+		jnz .delay
+
+	xor al, al
+	out dx, al
+	popa
+	ret
 draw:
 	pusha
 	mov ax, OFFSET
 	mov es, ax
-	mov dl, 0x4F
 	call update
 	call clear_scr
+	mov dl, 0x4F
 	call draw_paddle
+	
+	mov dl, 0xC
+	call draw_ball
 	popa
 	iret
 
